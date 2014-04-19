39340cd6f65d01f9499e2354c763e2dec98e81ef
Patrick <patrick@patrick-virtual-machine.(none)>
2014-04-20 00:21:17 +0200

    Added list of captures in home page + link to show capture page

--- Capture.py
+++ Capture.py
@@ -1,14 +1,19 @@
 from subprocess import Popen
 from shutil import copyfile
 import time
+from os import listdir
+
+CURRENT_CAPTURE_PATH = 'captures/current'
+LIST_CAPTURES_PATH = 'captures/list'
 
 class Capture:
 	
-	def __init__(self):
+	def __init__(self, name = ''):
 		self.proc = None
+		self.name = name
 
 	def start(self):
-		self.proc = Popen(['tshark'], stdout=open('captures/current', 'w'))
+		self.proc = Popen(['tshark', 'port', '80'], stdout=open(CURRENT_CAPTURE_PATH, 'w'))
 		self.name = time.strftime('%d.%m.%Y %X')
 
 	def stop(self):
@@ -16,10 +21,18 @@ class Capture:
 			return
 		self.proc.terminate()
 		self.proc = None
-		copyfile('captures/current', 'captures/list/{0}'.format(self.name))		
+		copyfile(CURRENT_CAPTURE_PATH, '{0}/{1}'.format(LIST_CAPTURES_PATH, self.name))		
 
 	def is_running(self):
 		if self.proc == None:
 			return False
 		else:
 			return True
+
+	@staticmethod
+	def get_list_captures():
+		captures = list()
+		filenames = listdir(LIST_CAPTURES_PATH)
+		for filename in filenames :
+			captures.append(Capture(filename))
+		return captures
--- flaskNetworkAnalyzer.py
+++ flaskNetworkAnalyzer.py
@@ -9,25 +9,29 @@ app = Flask(__name__)
 current_capture = Capture()
 
 @app.route('/')
-def index():	
+def index():
 	if current_capture.is_running() == True:
-		return render_template('stop_capture.html')
+		return render_template('stop_capture.html', captures = Capture.get_list_captures())
 	else:
-	    return render_template('start_capture.html')	
+	    return render_template('start_capture.html', captures = Capture.get_list_captures())	
 
 @app.route('/capture/start', methods=['GET', 'POST'])
 def start_capture():
 	if current_capture.is_running() == True:
 		return index()
 	current_capture.start()
-	return render_template('stop_capture.html')	
+	return render_template('stop_capture.html', captures = Capture.get_list_captures())	
 
 @app.route('/capture/stop', methods=['GET', 'POST'])
 def stop_capture():
 	if current_capture.is_running() == False:
 		return index()
 	current_capture.stop()
-	return render_template('start_capture.html')
+	return render_template('start_capture.html', captures = Capture.get_list_captures())
+
+@app.route('/<capture_name>', methods=['GET'])
+def show_capture(capture_name):
+	return render_template('show_capture.html')
 
 if __name__ == '__main__':
     app.run(debug=DEBUG) 
